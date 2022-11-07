1f3f103fa3df986949088e9070213386ef19d773
alan13579 <apple5478963210@gmail.com>
2022-11-07 18:11:58 +0800

    管理员可删除用户

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,9 +17,13 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            //'email_verified_at' => now(),
+            'email_verified_at' => $this->faker->dateTimeThisDecade(),
+            //'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => $this->faker->password(), // password
             'remember_token' => Str::random(10),
+            'created_at' => $this->faker->dateTime(),
+            'updated_at' => $this->faker->dateTimeThisYear(),
         ];
     }
 
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -20,6 +20,7 @@ class UsersTableSeeder extends Seeder
         $user->name = 'Summer';
         $user->email = 'summer@example.com';
 		$user->password = bcrypt('123456');
+		$user->is_admin = true;
         $user->save();
     }
 }
